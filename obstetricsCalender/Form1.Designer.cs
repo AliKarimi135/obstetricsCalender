2b78989e47e42bab214c33e694110cad9e027083
ali karimi <ali.karimi2138@gmail.com>
2020-06-21 20:06:31 +0430

    finish program

---
+++ obstetricsCalender/Form1.Designer.cs
@@ -0,0 +1,433 @@
+﻿namespace obstetricsCalender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlmp = new System.Windows.Forms.Button();
+            this.resultLmp = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.resultWeak1 = new System.Windows.Forms.Label();
+            this.resultWeakLpm = new System.Windows.Forms.Label();
+            this.sumday = new System.Windows.Forms.Label();
+            this.lblMonth = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.datePikerLmp = new Atf.UI.DateTimeSelector();
+            this.label4 = new System.Windows.Forms.Label();
+            this.datepikerlab = new Atf.UI.DateTimeSelector();
+            this.selectWeak = new System.Windows.Forms.ComboBox();
+            this.selectDay = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.reultsono = new System.Windows.Forms.Label();
+            this.resultw = new System.Windows.Forms.Label();
+            this.resultw2 = new System.Windows.Forms.Label();
+            this.resultday = new System.Windows.Forms.Label();
+            this.resultmonth = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(390, 81);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label1.Size = new System.Drawing.Size(210, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(LMP)تاریخ اولین روز از آخرین قاعدگی ";
+            // 
+            // btnlmp
+            // 
+            this.btnlmp.Location = new System.Drawing.Point(33, 72);
+            this.btnlmp.Name = "btnlmp";
+            this.btnlmp.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnlmp.Size = new System.Drawing.Size(114, 25);
+            this.btnlmp.TabIndex = 2;
+            this.btnlmp.Text = "محاسبه";
+            this.btnlmp.UseVisualStyleBackColor = true;
+            this.btnlmp.Click += new System.EventHandler(this.btnlmp_Click);
+            // 
+            // resultLmp
+            // 
+            this.resultLmp.AutoSize = true;
+            this.resultLmp.Location = new System.Drawing.Point(206, 125);
+            this.resultLmp.Name = "resultLmp";
+            this.resultLmp.Size = new System.Drawing.Size(0, 13);
+            this.resultLmp.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(446, 287);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "تاریخ انجام آزمایش سونوگرافی";
+            // 
+            // resultWeak1
+            // 
+            this.resultWeak1.AutoSize = true;
+            this.resultWeak1.Location = new System.Drawing.Point(206, 148);
+            this.resultWeak1.Name = "resultWeak1";
+            this.resultWeak1.Size = new System.Drawing.Size(0, 13);
+            this.resultWeak1.TabIndex = 5;
+            // 
+            // resultWeakLpm
+            // 
+            this.resultWeakLpm.AutoSize = true;
+            this.resultWeakLpm.Location = new System.Drawing.Point(206, 174);
+            this.resultWeakLpm.Name = "resultWeakLpm";
+            this.resultWeakLpm.Size = new System.Drawing.Size(0, 13);
+            this.resultWeakLpm.TabIndex = 6;
+            // 
+            // sumday
+            // 
+            this.sumday.AutoSize = true;
+            this.sumday.Location = new System.Drawing.Point(206, 196);
+            this.sumday.Name = "sumday";
+            this.sumday.Size = new System.Drawing.Size(0, 13);
+            this.sumday.TabIndex = 7;
+            // 
+            // lblMonth
+            // 
+            this.lblMonth.AutoSize = true;
+            this.lblMonth.Location = new System.Drawing.Point(206, 218);
+            this.lblMonth.Name = "lblMonth";
+            this.lblMonth.Size = new System.Drawing.Size(0, 13);
+            this.lblMonth.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(225, 47);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // datePikerLmp
+            // 
+            this.datePikerLmp.Location = new System.Drawing.Point(209, 76);
+            this.datePikerLmp.Name = "datePikerLmp";
+            this.datePikerLmp.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.datePikerLmp.Size = new System.Drawing.Size(149, 21);
+            this.datePikerLmp.TabIndex = 10;
+            this.datePikerLmp.UsePersianFormat = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(353, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(244, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "سن بارداری محاسبه شده توسط آزمایش سونوگرافی";
+            // 
+            // datepikerlab
+            // 
+            this.datepikerlab.Location = new System.Drawing.Point(174, 279);
+            this.datepikerlab.Name = "datepikerlab";
+            this.datepikerlab.Size = new System.Drawing.Size(161, 21);
+            this.datepikerlab.TabIndex = 12;
+            this.datepikerlab.UsePersianFormat = true;
+            // 
+            // selectWeak
+            // 
+            this.selectWeak.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectWeak.FormattingEnabled = true;
+            this.selectWeak.Items.AddRange(new object[] {
+            "1 هفته",
+            "2 هفته",
+            "3 هفته",
+            "4 هفته",
+            "5 هفته",
+            "6 هفته",
+            "7 هفته",
+            "8 هفته",
+            "9 هفته",
+            "10 هفته",
+            "11 هفته",
+            "12 هفته",
+            "13 هفته",
+            "14 هفته",
+            "15 هفته",
+            "16 هفته",
+            "17 هفته",
+            "18 هفته",
+            "19 هفته",
+            "20 هفته",
+            "21 هفته",
+            "22 هفته",
+            "23 هفته",
+            "24 هفته",
+            "25 هفته",
+            "26 هفته",
+            "27 هفته",
+            "28 هفته",
+            "29 هفته",
+            "30 هفته",
+            "31 هفته",
+            "32 هفته",
+            "33 هفته",
+            "34 هفته",
+            "35 هفته",
+            "36 هفته",
+            "37 هفته",
+            "38 هفته",
+            "39 هفته",
+            "40 هفته"});
+            this.selectWeak.Location = new System.Drawing.Point(256, 320);
+            this.selectWeak.Name = "selectWeak";
+            this.selectWeak.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.selectWeak.Size = new System.Drawing.Size(79, 21);
+            this.selectWeak.TabIndex = 13;
+            // 
+            // selectDay
+            // 
+            this.selectDay.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.selectDay.FormattingEnabled = true;
+            this.selectDay.Items.AddRange(new object[] {
+            "0 روز",
+            "1 روز",
+            "2 روز",
+            "3 روز",
+            "4 روز",
+            "5 روز",
+            "6 روز"});
+            this.selectDay.Location = new System.Drawing.Point(174, 320);
+            this.selectDay.Name = "selectDay";
+            this.selectDay.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.selectDay.Size = new System.Drawing.Size(67, 21);
+            this.selectDay.TabIndex = 14;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "محاسبه";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // reultsono
+            // 
+            this.reultsono.AutoSize = true;
+            this.reultsono.Location = new System.Drawing.Point(171, 372);
+            this.reultsono.Name = "reultsono";
+            this.reultsono.Size = new System.Drawing.Size(0, 13);
+            this.reultsono.TabIndex = 17;
+            // 
+            // resultw
+            // 
+            this.resultw.AutoSize = true;
+            this.resultw.Location = new System.Drawing.Point(171, 401);
+            this.resultw.Name = "resultw";
+            this.resultw.Size = new System.Drawing.Size(0, 13);
+            this.resultw.TabIndex = 18;
+            // 
+            // resultw2
+            // 
+            this.resultw2.AutoSize = true;
+            this.resultw2.Location = new System.Drawing.Point(171, 429);
+            this.resultw2.Name = "resultw2";
+            this.resultw2.Size = new System.Drawing.Size(0, 13);
+            this.resultw2.TabIndex = 19;
+            // 
+            // resultday
+            // 
+            this.resultday.AutoSize = true;
+            this.resultday.Location = new System.Drawing.Point(171, 457);
+            this.resultday.Name = "resultday";
+            this.resultday.Size = new System.Drawing.Size(0, 13);
+            this.resultday.TabIndex = 20;
+            // 
+            // resultmonth
+            // 
+            this.resultmonth.AutoSize = true;
+            this.resultmonth.Location = new System.Drawing.Point(171, 483);
+            this.resultmonth.Name = "resultmonth";
+            this.resultmonth.Size = new System.Drawing.Size(0, 13);
+            this.resultmonth.TabIndex = 21;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(-68, 242);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(934, 13);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "_________________________________________________________________________________" +
+    "_________________________________________________________________________-";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(-149, 506);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(934, 13);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "_________________________________________________________________________________" +
+    "_________________________________________________________________________-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(300, 530);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(284, 13);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "برای استفاده از این برنامه تاریخ سیستم خود را تنظیم کنید  **";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(84, 554);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(487, 13);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "   منتشر شده است و برنامه نویس هیچ گونه مسولیت حقوقی و کیفری در  open source این " +
+    "برنامه به صورت";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(381, 576);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(187, 13);
+            this.label9.TabIndex = 26;
+            this.label9.Text = "قبال استفاده  دیگران از این برنامه ندارد";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label10.Location = new System.Drawing.Point(569, 561);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(31, 13);
+            this.label10.TabIndex = 27;
+            this.label10.Text = "******";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(206, 9);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(172, 16);
+            this.label11.TabIndex = 28;
+            this.label11.Text = "محاسبه تاریخ تقریبی زایمان";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 596);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.resultmonth);
+            this.Controls.Add(this.resultday);
+            this.Controls.Add(this.resultw2);
+            this.Controls.Add(this.resultw);
+            this.Controls.Add(this.reultsono);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.selectDay);
+            this.Controls.Add(this.selectWeak);
+            this.Controls.Add(this.datepikerlab);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.datePikerLmp);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblMonth);
+            this.Controls.Add(this.sumday);
+            this.Controls.Add(this.resultWeakLpm);
+            this.Controls.Add(this.resultWeak1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resultLmp);
+            this.Controls.Add(this.btnlmp);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "                                                    محاسبه تاریخ تقریبی زایمان   " +
+    "                                            ";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlmp;
+        private System.Windows.Forms.Label resultLmp;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label resultWeak1;
+        private System.Windows.Forms.Label resultWeakLpm;
+        private System.Windows.Forms.Label sumday;
+        private System.Windows.Forms.Label lblMonth;
+        private System.Windows.Forms.Label label3;
+        private Atf.UI.DateTimeSelector datePikerLmp;
+        private System.Windows.Forms.Label label4;
+        private Atf.UI.DateTimeSelector datepikerlab;
+        private System.Windows.Forms.ComboBox selectWeak;
+        private System.Windows.Forms.ComboBox selectDay;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label reultsono;
+        private System.Windows.Forms.Label resultw;
+        private System.Windows.Forms.Label resultw2;
+        private System.Windows.Forms.Label resultday;
+        private System.Windows.Forms.Label resultmonth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
